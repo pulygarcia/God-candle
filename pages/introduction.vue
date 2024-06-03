6c476114c589db3fc79170f4289118ef1474de90
puly garcia <pulygarcia09@gmail.com>
2024-06-03 02:11:31 -0300

    finished: introduction page

---
+++ pages/introduction.vue
@@ -0,0 +1,63 @@
+<template>
+    <Header></Header>
+    
+    <div class="container mx-auto w-11/12 md:w-full py-8">
+      <h1 class="text-center text-5xl py-16 font-bold">Introducción a criptomonedas y blockchain</h1>
+  
+      <article class="space-y-10">
+        <div>
+          <h2 class="text-4xl font-semibold mb-6">¿Qué son las criptomonedas?</h2>
+          <p class="text-lg">Las <strong>criptomonedas</strong> son monedas digitales o virtuales que utilizan <strong>tecnología blockchain</strong> para garantizar transacciones seguras y controlar la creación de nuevas unidades. A diferencia de las monedas tradicionales emitidas por los gobiernos, las criptomonedas operan de manera descentralizada a través de una tecnología llamada blockchain, que es un libro de contabilidad público y distribuido. Siendo Bitcoin (BTC) y Ethereum (ETH) las criptomonedas más conocidas y longevas del mercado.</p>
+        </div>
+
+        <div class="max-w-96 overflow-hidden">
+            <img src="https://www.criptonoticias.com/wp-content/uploads/2023/02/criptomonedas-top-suben-precio-1140x570.jpg" alt="criptomonedas">
+        </div>
+
+        <div>
+          <h2 class="text-4xl font-semibold mb-6">Blockchain</h2>
+          <p class="text-lg">La blockchain, o cadena de bloques, es una tecnología de registro descentralizado que permite llevar un control seguro y transparente de transacciones y datos. Funciona como un libro de contabilidad digital distribuido en una red de computadoras (nodos), donde cada bloque contiene un conjunto de transacciones verificadas y está vinculado al bloque anterior mediante técnicas criptográficas.</p>
+          <p class="text-lg">A continuación verás características claves acerca de esta tecnología:</p>
+
+          <ul class="list-disc mt-5 space-y-3">
+            <li>
+                <p><strong>Descentralización</strong>: No existe una autoridad central que controle la red; en su lugar, todos los nodos participantes mantienen una copia del registro completo.</p>
+            </li>
+            <li>
+                <p><strong>Transparencia</strong>: Todas las transacciones son visibles para los participantes de la red y pueden ser verificadas por cualquiera.</p>
+            </li>
+            <li>
+                <p><strong>Inmutabilidad</strong>: Una vez que un bloque se agrega a la cadena, no puede ser modificado ni eliminado, lo que asegura la integridad de los datos.</p>
+            </li>
+            <li>
+                <p><strong>Seguridad</strong>: La criptografía asegura que las transacciones sean autenticas y que los datos en la blockchain sean a prueba de manipulaciones.</p>
+            </li>
+
+          </ul>
+        </div>
+  
+        <div>
+          <h2 class="text-4xl font-semibold mb-6">Volatilidad</h2>
+          <p class="text-lg">La volatilidad de las criptomonedas se refiere a la rapidez y magnitud con la que sus precios pueden cambiar en un corto período de tiempo. Esta característica es una de las más distintivas y a menudo discutidas del mercado de criptomonedas.</p>
+          <p class="text-lg">Si bien existen criptomonedas estables como USDT (de la empresa Tether) o USDC (de Coinbase), diseñadas para mantener un valor fijo y reducir la volatilidad, la mayoría de las criptomonedas siguen siendo altamente volátiles. Estas stablecoins están respaldadas por activos reales o algoritmos para mantener su paridad con el dólar estadounidense, proporcionando una opción más segura para los inversores que buscan protegerse de las fluctuaciones de precio extremas. Sin embargo, es importante tener en cuenta que, aunque las stablecoins ofrecen mayor estabilidad, la mayoría del mercado de criptomonedas sigue sujeto a movimientos de precios impredecibles y significativos. <span class="text-red-500">Sobre todo en el caso de los activos con baja capitalización de mercado</span>.</p>
+        </div>
+
+        <div>
+          <h2 class="text-4xl font-semibold mb-6">Altcoins</h2>
+          <p class="text-lg">Las altcoins son todas las criptomonedas que existen <strong>excluyendo a Bitcoin</strong>. El término "altcoin" es una abreviatura de "alternative coin" (moneda alternativa) y se refiere a cualquier criptomoneda que no sea Bitcoin. Desde la creación de Bitcoin en 2009, miles de altcoins han surgido, cada una con sus propias características y propósitos. Las altcoins poseen una capitalización de mercado mucho menor a la que tiene Bitcoin, es por eso que los movimientos más volátiles tanto al alza como a la baja se producen en ellas, y es donde se suele corrrer más riesgo al invertir en ellas por la misma razón. Es clave invertir en proyectos serios y con propósitos innovadores para evitar perder dinero en criptomonedas de dudosa reputación o en proyectos que carecen de una visión clara y sostenible.</p>
+        </div>
+
+        <div>
+          <h2 class="text-4xl font-semibold mb-6">Noticias o eventos</h2>
+          <p class="text-lg">Las criptomonedas son altamente sensibles a las noticias. Anuncios de regulaciones gubernamentales, hackeos de exchanges, o adopciones por parte de grandes empresas pueden causar grandes fluctuaciones en los precios. <strong>Es un hecho que los mayores movimientos en el mercado son producidos gran cantidad de veces por noticias, rumores, narrativas, o eventos</strong>.</p>
+        </div>
+
+        <div>
+          <h2 class="text-4xl font-semibold mb-6">Aplicaciones</h2>
+          <p class="text-lg">Existen varias aplicaciones y plataformas que permiten a los usuarios invertir en criptomonedas. Descubre cuales son con un click <NuxtLink to="/">AQUÍ</NuxtLink>. Estas plataformas suelen ofrecer una variedad de funciones que van desde la compra y venta de criptomonedas hasta el seguimiento de precios en tiempo real y la gestión de carteras. Además de contar con otros beneficios como tener una empresa custodiando tu dinero de forma segura y ofrecer herramientas para hacer trading y educar a quienes lo necesiten.</p>
+        </div>
+      </article>
+    </div>
+  
+    <Footer></Footer>
+  </template>
