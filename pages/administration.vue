4b575b60984dd5060cbf140efad3352787ba088d
puly garcia <pulygarcia09@gmail.com>
2024-06-03 21:29:20 -0300

    creating files for the rest of the entries, leaving them with feedback

---
+++ pages/administration.vue
@@ -0,0 +1,33 @@
+<template>
+    <Header></Header>
+    
+    <div class="container mx-auto w-11/12 md:w-full py-8">
+      <h1 class="text-center text-5xl py-16 font-bold">Gestion y distribución de capital al invertir</h1>
+      <Writting />
+  
+      <!-- <article class="space-y-10">
+        <div>
+          <h2 class="text-4xl font-semibold mb-6">¿Qué es GodCandle?</h2>
+          <p class="text-lg"><strong>GodCandle</strong> es un blog dedicado a las inversiones en blockchain y criptomonedas. Útil tanto para nuevos interesados en el entorno como para personas que ya cuenten con experiencia en trading dentro del espacio. En el cual encontrarás información valiosa como consejos a la hora de diversificar tu capital, gestionar riesgo de acuerdo a tu tolerancia del mismo, plataformas altamente recomendadas, proyectos con gran proyección de crecimiento y estrategias.</p>
+        </div>
+  
+        <div>
+          <h2 class="text-4xl font-semibold mb-6">Objetivos</h2>
+          <p class="text-lg">Nuestro principal objetivo es proporcionar a posibles inversores y entusiastas de las criptomonedas un recurso confiable y completo para informarse y educarse sobre el mundo de las inversiones en blockchain y criptomonedas.</p>
+  
+          <ul class="list-disc ps-3 mt-6">
+            <li class="">Ofrecer contenido educativo y accesible para todos, independientemente de su nivel de experiencia, para que puedan tomar decisiones de inversión informadas.</li>
+            <li class="">Mantener a nuestros lectores actualizados con las últimas tendencias, desarrollos y oportunidades en el ámbito de la blockchain y las criptomonedas.</li>
+            <li class="">Construir una comunidad de potenciales inversores informados correctamente aprendiendo de experiencias y mejores prácticas para el éxito mutuo.</li>
+            <li class="">Proveer análisis y opiniones basadas en investigaciones rigurosas sobre las plataformas de trading más recomendadas y seguras.</li>
+          </ul>
+        </div>
+      </article> -->
+  
+      <div class="divider my-5"></div>
+  
+      <p class="mt-5">Este sitio fué construído por <a href="https://github.com/pulygarcia" class="text-emerald-500">Puly G</a>.</p>
+    </div>
+  
+    <Footer></Footer>
+  </template>
