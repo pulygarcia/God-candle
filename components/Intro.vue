78134cab53b8abd7963fc2f94220b07243259b58
puly garcia <pulygarcia09@gmail.com>
2024-06-09 02:50:45 -0300

    little image changes, adding live quotes section intro

--- components/Intro.vue
+++ components/Intro.vue
@@ -3,7 +3,7 @@ import GodCandleFloat from './GodCandleFloat.vue';
 <template>
     <div class="max-w-[1200px] mx-auto">
         <div class="w-11/12 mx-auto md:w-full py-8">
-            <img src="/public/chart.jpg" alt="trading" class="trading-image mx-auto rounded-xl -mt-20 md:mt-5 filter grayscale hover:grayscale-0 transition">
+            <img src="/public/trading.jpg" alt="trading" class="trading-image mx-auto rounded-xl -mt-20 md:mt-5 filter grayscale hover:grayscale-0 transition">
 
             <article class="mt-20 space-y-6 grid grid-cols-1 md:grid-cols-3 items-center">
                 <div class="col-span-2">
@@ -53,7 +53,7 @@ import GodCandleFloat from './GodCandleFloat.vue';
                         <button class="btn border border-emerald-500 hover:border-black transition w-56 text-base">Ver más</button>
                     </NuxtLink>
                 </div>
-                <NuxtImg format="webp" src="/candles.svg" alt="trading candles" loading="lazy" class="h-48 md:h-80 w-full object-contain rounded-md"/>
+                <NuxtImg format="webp" src="/blockchain.png" alt="blockchain" loading="lazy" class="h-48 md:h-80 w-full object-contain rounded-md"/>
             </article>
 
             <div class="divider my-5"></div>
@@ -101,6 +101,57 @@ import GodCandleFloat from './GodCandleFloat.vue';
                         <button class="btn border border-emerald-500 hover:border-black transition w-56 text-base">Ver más</button>
                     </NuxtLink>
                 </div>
+            </article>
+
+            <article class="mt-20 space-y-6 grid grid-cols-1 md:grid-cols-3 items-center">
+                <div class="col-span-2">
+                    <h3 class="text-emerald-500 uppercase font-bold">Cotizaciones del momento</h3>
+                    <h2 class="mt-6 mb-8 text-4xl lg:text-5xl  font-bold">Monitorea las cotizaciones en tiempo real de las 100 criptomonedas más valoradas del mercado</h2>
+                    <p class="mb-10 leading-loose text-gray-500 md:w-8/12">Mantenerse al tanto a la hora de invertir es crucial, especialmente en el mercado de criptomonedas. En esta sección puedes ver los siguientes datos de los activos: </p>
+
+                    <ul class="mb-6 ">
+                        <li class="mb-4 flex">
+                            <div class="rounded-full bg-transparent w-5 h-5 flex justify-center items-center mr-4">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-check" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#10b981" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <path d="M5 12l5 5l10 -10" />
+                                </svg>
+                            </div>
+                            <span>Precio</span>
+                        </li>
+                        <li class="mb-4 flex">
+                            <div class="rounded-full bg-transparent w-5 h-5 flex justify-center items-center mr-4">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-check" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#10b981" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <path d="M5 12l5 5l10 -10" />
+                                </svg>
+                            </div>
+                            <span>Capitalización de mercado</span>
+                        </li>
+                        <li class="mb-4 flex">
+                            <div class="rounded-full bg-transparent w-5 h-5 flex justify-center items-center mr-4">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-check" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#10b981" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <path d="M5 12l5 5l10 -10" />
+                                </svg>
+                            </div>
+                            <span>Volumen de trading en 24h</span>
+                        </li>
+                        <li class="mb-4 flex">
+                            <div class="rounded-full bg-transparent w-5 h-5 flex justify-center items-center mr-4">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-check" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#10b981" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <path d="M5 12l5 5l10 -10" />
+                                </svg>
+                            </div>
+                            <span>Ranking</span>
+                        </li>
+                    </ul>
+                    <NuxtLink to="/live-quotes">
+                        <button class="btn border border-emerald-500 hover:border-black transition w-56 text-base">Ver más</button>
+                    </NuxtLink>
+                </div>
+                <NuxtImg format="webp" src="/candles.svg" alt="trading candles" loading="lazy" class="h-48 md:h-80 w-full object-contain rounded-md"/>
             </article>
 
             <div class="divider my-5"></div>
