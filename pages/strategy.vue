2f06ab1a6271c63dccbb8c54551868fdcf9277e4
puly garcia <pulygarcia09@gmail.com>
2024-06-11 16:39:01 -0300

    adding some info into strategy section

--- pages/strategy.vue
+++ pages/strategy.vue
@@ -3,26 +3,28 @@
     
     <div class="container mx-auto w-11/12 md:w-full py-8">
       <h1 class="text-center text-5xl py-16 font-bold">Estrategia</h1>
-      <Writting />
   
-      <!-- <article class="space-y-10">
+      <article class="space-y-10">
         <div>
-          <h2 class="text-4xl font-semibold mb-6">¿Qué es GodCandle?</h2>
-          <p class="text-lg"><strong>GodCandle</strong> es un blog dedicado a las inversiones en blockchain y criptomonedas. Útil tanto para nuevos interesados en el entorno como para personas que ya cuenten con experiencia en trading dentro del espacio. En el cual encontrarás información valiosa como consejos a la hora de diversificar tu capital, gestionar riesgo de acuerdo a tu tolerancia del mismo, plataformas altamente recomendadas, proyectos con gran proyección de crecimiento y estrategias.</p>
+          <h2 class="text-4xl font-semibold mb-6">Entradas y salidas en los momentos adecuados</h2>
+          <p class="text-lg">La idea es comprar barato y vender caro, ¿no?. Es por eso que hay que llevar una estrategia adecuada y contar con fundamentos sólidos para tomar desiciones de inversión y saber cuando entrar, y cuando salir del mercado de las criptomonedas.</p>
         </div>
   
         <div>
-          <h2 class="text-4xl font-semibold mb-6">Objetivos</h2>
-          <p class="text-lg">Nuestro principal objetivo es proporcionar a posibles inversores y entusiastas de las criptomonedas un recurso confiable y completo para informarse y educarse sobre el mundo de las inversiones en blockchain y criptomonedas.</p>
+          <h2 class="text-4xl font-semibold mb-6">Dominancia</h2>
+          <p class="text-lg">El siguiente gráfico muestra en porcentaje la dominancia en conjunto de BTC, ETH, USDT y USDC dentro del mercado, y se puede encontrar el mismo realizando la búsqueda en trading view con estos símbolos: <strong>BTC.D+ETH.D+USDT.D+USDC.D</strong> . Al momento de escibir este artículo, este gráfico se encuentra al 79.50%, quiere decir que casi el 80% de <strong>todo el dinero dentro del mercado de las criptomonedas se encuentra en Bitcoin, Ethereum, USDT y USDC.</strong></p>
   
-          <ul class="list-disc ps-3 mt-6">
-            <li class="">Ofrecer contenido educativo y accesible para todos, independientemente de su nivel de experiencia, para que puedan tomar decisiones de inversión informadas.</li>
-            <li class="">Mantener a nuestros lectores actualizados con las últimas tendencias, desarrollos y oportunidades en el ámbito de la blockchain y las criptomonedas.</li>
-            <li class="">Construir una comunidad de potenciales inversores informados correctamente aprendiendo de experiencias y mejores prácticas para el éxito mutuo.</li>
-            <li class="">Proveer análisis y opiniones basadas en investigaciones rigurosas sobre las plataformas de trading más recomendadas y seguras.</li>
-          </ul>
+          <img src="/altcoins zones.PNG" alt="gráfico" class="my-6 md:h-96 object-cover">
+
+          <p class="text-lg">La flecha de caída que está mercada en la gráfica, representa el momento en el que la dominancia en conjunto de estos 4 activos cae, favoreciendo a las <strong>Altcoins</strong>, ya que significa fluído de dinero hacia el resto de las criptomonedas generando un gran movimiento alcista en ellas (te invito a corroborarlo viendo el precio de altcoins el día de la fecha que más baja estaba la dominancia de Btc,Eth, Usdt y Usdc según el gráfico). Sobre todo luego de haber tenido el <strong>Halving de Bitcoin</strong>, ya que históricamente luego de él, la dominancia de Bitcoin empieza a descender, pero su precio se incrementa al alza generando euforia y grandes movimientos en el resto de las criptomonedas.</p>
+
+          <p class="text-lg mt-6">Por otro lado, la dominancia alta de stable coins como USDT y USDC significa que la gente está protegiendo su capital en ellas esperando una oportunidad de entrada al mercado, que cuando se dé se verá reflejado en el precio de las <strong>Altcoins</strong> especialmente.</p>
+
+          <p class="text-lg mt-6">Es importante tener en cuenta que el gráfico es analizado en temporalidad SEMANAL, lo que lleva a que la caída que marca la fecha puede tardar meses en llegar al objetivo, siendo que una vela del grafico representa una semana.</p>
+
+          <Writting />
         </div>
-      </article> -->
+      </article>
   
       <div class="divider my-5"></div>
   
